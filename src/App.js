e4e774d9f39cf87b7983155a2a05fc50b627ac79
leesoyuun <leesoyun4206@naver.com>
2022-10-24 21:22:16 +0900

    Test react

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "React";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi I'm {name}
         </p>
         <a
           className="App-link"
