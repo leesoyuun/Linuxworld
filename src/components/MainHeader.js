0704e7a3fcdecad004280e93d43749509dd95ecd
leesoyuun <leesoyun4206@naver.com>
2022-10-24 23:07:35 +0900

    Make main header frame

---
+++ src/components/MainHeader.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './MainHeader.css';
+
+function mainheader(){
+    return(
+        <header className='mainheader'>
+            <nav className='nav'>
+                <img id='nav-logo' src={require('../assets/img/Logo.jpg')}/>
+                <input></input>
+                <ul>
+                    <img id='check' src={require('../assets/img/check-mark.png')}/>
+                    <li>출첵</li>
+                </ul>
+                <ul>
+                    <img id='like' src={require('../assets/img/heart.png')}/>
+                    <li>좋아요</li>
+                </ul>
+                <ul>
+                    <img id='cart' src={require('../assets/img/shopping-cart.png')}/>
+                    <li>장바구니</li>
+                </ul>
+                <ul>
+                    <img id='fashion' src={require('../assets/img/fashion.png')}/>
+                    <li>기획전</li>
+                </ul>
+                
+            </nav>
+        </header>
+    )
+}
+export default mainheader
