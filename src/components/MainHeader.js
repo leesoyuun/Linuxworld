4dcfe3cc25a0eec15ddb4ea0feec5c08b7238989
leesoyuun <leesoyun4206@naver.com>
2022-10-25 22:55:31 +0900

    Add MainHeader style except popular search

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -6,7 +6,7 @@ function mainheader(){
         <header className='mainheader'>
             <nav className='nav'>
                 <img id='nav-logo' src={require('../assets/img/Logo.jpg')}/>
-                <input></input>
+                <input id='input'/>
                 <ul>
                     <img id='check' src={require('../assets/img/check-mark.png')}/>
                     <li>출첵</li>
@@ -23,7 +23,6 @@ function mainheader(){
                     <img id='fashion' src={require('../assets/img/fashion.png')}/>
                     <li>기획전</li>
                 </ul>
-                
             </nav>
         </header>
     )
