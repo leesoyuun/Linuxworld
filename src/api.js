c0018f4261b2490688a76429e91d97d740072d6b
Junanjunan <taltalrealty@gmail.com>
2022-10-31 01:47:26 +0900

    Axios: refactoring 2

--- src/api.js
+++ src/api.js
@@ -5,7 +5,4 @@ const Instance = axios.create({
     baseURL: "http://127.0.0.1:8000/api/v1/"
 })
 
-export async function getProfile(){
-    const response = await Instance.get("profile/1/");
-    return response.data;
-};
+export const getProfile = () => Instance.get("profile/1/").then(response => response.data);
