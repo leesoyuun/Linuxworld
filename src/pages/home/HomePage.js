425fbdbac41f5ac0c45d424d74eff703c8d41c34
Junanjunan <taltalrealty@gmail.com>
2022-10-31 00:34:04 +0900

    Test manual fetch example

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/MainHeader'
 
 function HomePage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [profile, setProfile] = useState();
+    const fetchProfile = async () => {
+        const response = await fetch("http://127.0.0.1:8000/api/v1/profile/1/");
+        const json = await response.json();
+        setProfile(json);
+        setIsLoading(false);
+    };
+    useEffect(() => {
+        fetchProfile();
+    }, []);
+    console.log(profile);
     return(
+
         <div>
-            <Header/>
+            {isLoading ? null : <Header/> }
         </div>
         
     )
