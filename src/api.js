0d053be483df5cea1f9959534d6ba130a028dd55
Junanjunan <taltalrealty@gmail.com>
2022-10-31 01:42:47 +0900

    Axios install and refatory

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
+import axios from "axios";
+
+
 export async function getProfile(){
-    const response = await fetch("http://127.0.0.1:8000/api/v1/profile/1/");
-    const json = await response.json();
-    return json;
+    const response = await axios.get("http://127.0.0.1:8000/api/v1/profile/1/");
+    return response.data;
 };
