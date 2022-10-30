c36d228b5a683c9ae973d6bc5c3e822801223031
Junanjunan <taltalrealty@gmail.com>
2022-10-31 01:32:13 +0900

    Add useQuery with code refactoring

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,27 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query';
+import { getProfile } from '../../api';
 import Header from '../../components/MainHeader'
 import SubHeader from '../../components/SubHeader'
 import './HomePage.css'
 
 function HomePage() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [profile, setProfile] = useState();
-    const fetchProfile = async () => {
-        const response = await fetch("http://127.0.0.1:8000/api/v1/profile/1/");
-        const json = await response.json();
-        setProfile(json);
-        setIsLoading(false);
-    };
-    useEffect(() => {
-        fetchProfile();
-    }, []);
-    console.log(profile);
+    const { isLoading, data } = useQuery(["profile"], getProfile);
+    console.log(data);
     return(
         <div className='home_header'>
             <Header/>
             <SubHeader/>
         </div>
-        
+
     )
 }
 export default HomePage
