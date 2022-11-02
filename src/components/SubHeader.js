e5efae1638e0ad386a77cc9a43df7d44f45971c0
leesoyuun <leesoyun4206@naver.com>
2022-11-02 23:14:07 +0900

    Modify sub header position and background color

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -5,10 +5,22 @@ function subheader(){
         <header className='subheader'>
             <nav className='subnav'>
                 <ul id='sub_ul'>
-                    <img className='sub_btn' src={require('../assets/img/menu.png')}/>
+                    <li>
+                        <img className='sub_btn' src={require('../assets/img/menu.png')}/>
+                    </li>
                     <li>베스트</li>
                     <li>신상</li>
-                    <li>아우터</li>
+                    <li>
+                        아우터
+                        {/* <ul id='sub_outer_hide'>
+                            <li>가디건/조끼</li>
+                            <li>야상/점퍼</li>
+                            <li>자켓/코트</li>
+                            <li>패딩</li>
+                            <li>플리스</li>
+                        </ul> */}
+                    </li>
+                        
                     <li>상의</li>
                     <li>셔츠/블라우스</li>
                     <li>트레이닝</li>
@@ -18,8 +30,10 @@ function subheader(){
                     <li>팬츠</li>
                     <li>가방</li>
                     <li>악세사리</li>
-                    <div id='today_start'>오늘출발</div>
                 </ul>
+                <div id='today_start'>
+                    <a id='today_start_text'>오늘 출발</a>
+                </div>
             </nav>
         </header>
     )
