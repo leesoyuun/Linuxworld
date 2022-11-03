529ed58cf5daa91730f665be63fb8a81f78b3776
Junanjunan <taltalrealty@gmail.com>
2022-11-03 22:13:59 +0900

    login status check

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/v1/"
+    baseURL: "http://127.0.0.1:8000/api/v1/",
+    withCredentials: true
 })
 
 export const getProfile = () => instance.get("profile/1/").then(response => response.data);
+export const getMe = () => instance.get("me/").then(response => response.data);
--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,12 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
-import { getProfile } from '../../api';
+import { getProfile, getMe } from '../../api';
 import Header from '../../components/MainHeader'
 import SubHeader from '../../components/SubHeader'
 import './HomePage.css'
 
 function HomePage() {
-    const { isLoading, data } = useQuery(["profile"], getProfile);
-    console.log(data);
+    const { isLoading, data, isError } = useQuery(["me"], getMe, {
+        retry: false
+    });
+    // console.log(data);
+    console.log(useQuery(["me"], getMe, {
+        retry: false
+    }))
     return(
         <div className='home_header'>
             <Header/>
